d1594f9992e97514ccb59e592df0e9c4dec48576
katTheCatmom <katrine.hoel@gmail.com>
2025-10-11 15:21:00 +0200

    dependencies, files and application
    Opprettet mappestruktur i resources for static og vue. Opprettet en Application-klasse for å kunne teste at ting fungerer slik det skal.

---
+++ src/main/java/Application.java
@@ -0,0 +1,18 @@
+import io.javalin.Javalin;
+import io.javalin.vue.VueComponent;
+
+//For å få tilgang til bildefiler:
+import io.javalin.http.staticfiles.Location;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Javalin app = Javalin.create(javalinConfig -> {
+            javalinConfig.staticFiles.enableWebjars();
+            javalinConfig.staticFiles.add("/static", Location.CLASSPATH); //For å kunne hente ressurser fra static
+            javalinConfig.vue.vueInstanceNameInJs = "app";
+        }).start();
+
+        app.get("/", new VueComponent("search-page"));
+    }
+}
---
+++ src/main/resources/vue/view/search-page.vue
@@ -0,0 +1,13 @@
+<template id="search-page-template">
+    <main>
+      <h1>Hvor ønsker du å reise?</h1>
+    </main>
+</template>
+<link rel="stylesheet" href="/css/style.css">
+
+<script>
+  app.component("search-page", {
+    template: "#search-page-template"
+  });
+
+</script>
