d67f19b052066d9338bf328004551ea09844777b
katTheCatmom <katrine.hoel@gmail.com>
2025-10-11 16:04:09 +0200

    ferdig konvert
    Arbeidet gjort for søkesiden i HTML og CSS er nå "konvertert" til vue.

--- src/main/resources/vue/view/search-page.vue
+++ src/main/resources/vue/view/search-page.vue
@@ -1,9 +1,22 @@
 <template id="search-page-template">
-    <main>
-      <h1>Hvor ønsker du å reise?</h1>
-    </main>
+    <h1>Hvor ønsker du å reise?</h1>
+    <form action="" method="get">
+      <label for="start">Fra:
+        <input type="text" id="start" placeholder="Hvor ønsker du å reise fra?">
+      </label>
+      <label for="end">Til:
+        <input type="text" id="end" placeholder="Hvor ønsker du å reise til?">
+      </label>
+      <input id="search" type="submit" value="Finn Reise">
+    </form>
+
+    <section id="search-results">
+      <h2>Søkeresultat</h2>
+      <div class="result"></div>
+    </section>
 </template>
-<link rel="stylesheet" href="/css/style.css">
+<link rel="stylesheet" href="/css/global-style.css">
+<link rel="stylesheet" href="/css/search-page.css">
 
 <script>
   app.component("search-page", {
