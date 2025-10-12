a32026efee7200dd7353c2b44c9bc2cb976cadd1
katTheCatmom <katrine.hoel@gmail.com>
2025-10-12 19:08:14 +0200

    Added filtering
    Lagt til filtrering for transporttype (buss, trikk, tog, etc).

--- src/main/resources/vue/view/search-page.vue
+++ src/main/resources/vue/view/search-page.vue
@@ -1,12 +1,42 @@
 <template id="search-page-template">
     <h1>Hvor ønsker du å reise?</h1>
     <form action="" method="get">
-      <label for="start">Fra:
+      <label for="start" id="startLabel">Fra:
         <input type="text" id="start" placeholder="Hvor ønsker du å reise fra?">
       </label>
-      <label for="end">Til:
+      <label for="end" id="endLabel">Til:
         <input type="text" id="end" placeholder="Hvor ønsker du å reise til?">
       </label>
+
+      <!--HAMBURGER-->
+      <section id="filtering">
+        <div id="filterCollection">
+          <div id="group" class="filterButton">
+            <button id="ham" class="icon-btn"><i class="fa-solid fa-sliders"></i></button>
+          </div>
+          <!-- Transportvalg -->
+          <div id="menu" class="hidden">
+            <button id="close">X</button>
+            <label class="transportType">
+              Buss
+              <input type="checkbox">
+            </label>
+            <label class="transportType">
+              Tog
+              <input type="checkbox">
+            </label>
+            <label class="transportType">
+              Trikk
+              <input type="checkbox">
+            </label>
+            <label class="transportType">
+              Ferge
+              <input type="checkbox">
+            </label>
+          </div>
+        </div>
+      </section>
+
       <input id="search" type="submit" value="Finn Reise">
     </form>
 
@@ -22,5 +52,4 @@
   app.component("search-page", {
     template: "#search-page-template"
   });
-
 </script>
