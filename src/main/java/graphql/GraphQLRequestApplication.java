efbec1646ed7bf462961fa4fbcea2c053b232213
aymanbb <aymancool30@gmail.com>
2025-10-17 01:20:15 +0200

    Refactor av GraphQLQuery og implementasjon
    Jeg la til klassen QueryObject for å lage klasse objekter som inneholder variabler for bruk i queries.
    
    Dette forandrer ikke logikken til GraphQLQuery klassen (bare en konstruktør), men endrer implementasjonen litt.
    
    Dette er et bedre løsning, og bidrar til å gjøre implementasjonen av forsinkelsesvarsling lettere, dersom informasjonen som brukeren har satt inn blir lagret (i sesjonen) og kan brukes til å sende videre forespørsler om tidsoppdateringer etter den første forespørselen.

--- src/main/java/graphql/GraphQLRequestApplication.java
+++ src/main/java/graphql/GraphQLRequestApplication.java
@@ -2,6 +2,7 @@ package graphql;
 
 import graphql.client.GraphQLClient;
 import graphql.query.GraphQLQuery;
+import graphql.query.QueryObject;
 import org.gruppe4.enums.TransportType;
 
 import java.time.OffsetDateTime;
@@ -9,23 +10,32 @@ import java.util.ArrayList;
 
 public class GraphQLRequestApplication {
     public static void main(String[] args) throws Exception {
-        GraphQLClient client = new GraphQLClient();
-        GraphQLQuery query = new GraphQLQuery();
-
         // Lager automatiserte forespørsler senere når vi får brukerinput fra nettsiden
         // Lager klasse for å oversette navn på busstopper til ID senere
+
+        GraphQLClient client = new GraphQLClient();
+
+        OffsetDateTime customTime = OffsetDateTime.parse("2025-10-16T16:00:00.218+02:00");
 
         // Eksempel viaTrip query
         TransportType buss = TransportType.BUS; // enum
         TransportType tog = TransportType.RAIL; // enum
-
-        OffsetDateTime customTime = OffsetDateTime.parse("2025-10-15T16:00:00.218+02:00");
         ArrayList<String> transportModes = new ArrayList<>();
         transportModes.add(buss.getMode()); // bruker BUS enum
         transportModes.add(tog.getMode()); // bruker RAIL enum
 
+        QueryObject queryObject = new QueryObject(60053, 58794, customTime, transportModes);
+        GraphQLQuery query = new GraphQLQuery(queryObject);
+
+        /*
         // Stop ID 60053: Halden stasjon, stop ID 58794: Fredrikstad stasjon
         String response = client.sendGraphQLRequest(query.getQuery(60045, 59872, 62339, customTime, transportModes));
+        */
+
+        //String response = client.sendGraphQLRequest(query.getQuery(query.getFromStop(), query.getToStop(), customTime, transportModes));
+
+        String response = client.sendGraphQLRequest(query.getQuery(queryObject.getFromStop(), queryObject.getToStop(),
+                                                    queryObject.getDateTime(), queryObject.getTransportModes()));
 
         System.out.println(response);
     }
---
+++ src/main/java/graphql/query/QueryObject.java
@@ -0,0 +1,116 @@
+package graphql.query;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+
+public class QueryObject {
+    private int fromStop;
+    private int toStop;
+    private int viaStop;
+    private OffsetDateTime dateTime;
+    private String transportMode;
+    private ArrayList<String> transportModes;
+
+    public QueryObject(int fromStop, int toStop) {
+        this.fromStop = fromStop;
+        this.toStop = toStop;
+    }
+
+    public QueryObject(int fromStop, int toStop, OffsetDateTime dateTime) {
+        this.fromStop = fromStop;
+        this.toStop = toStop;
+        this.dateTime = dateTime;
+    }
+
+    public QueryObject(int fromStop, int toStop, String transportMode) {
+        this.fromStop = fromStop;
+        this.toStop = toStop;
+        this.transportMode = transportMode;
+    }
+
+    public QueryObject(int fromStop, int toStop, OffsetDateTime dateTime, String transportMode) {
+        this.fromStop = fromStop;
+        this.toStop = toStop;
+        this.dateTime = dateTime;
+        this.transportMode = transportMode;
+    }
+
+    public QueryObject(int fromStop, int toStop, ArrayList<String> transportModes) {
+        this.fromStop = fromStop;
+        this.toStop = toStop;
+        this.transportModes = transportModes;
+    }
+
+    public QueryObject(int fromStop, int toStop, OffsetDateTime dateTime, ArrayList<String> transportModes) {
+        this.fromStop = fromStop;
+        this.toStop = toStop;
+        this.dateTime = dateTime;
+        this.transportModes = transportModes;
+    }
+
+    public QueryObject(int fromStop, int toStop, int viaStop) {
+        this.fromStop = fromStop;
+        this.toStop = toStop;
+        this.viaStop = viaStop;
+    }
+
+    public QueryObject(int fromStop, int toStop, int viaStop, OffsetDateTime dateTime) {
+        this.fromStop = fromStop;
+        this.toStop = toStop;
+        this.viaStop = viaStop;
+    }
+
+    public QueryObject(int fromStop, int toStop, int viaStop, String transportMode) {
+        this.fromStop = fromStop;
+        this.toStop = toStop;
+        this.viaStop = viaStop;
+        this.transportMode = transportMode;
+    }
+
+    public QueryObject(int fromStop, int toStop, int viaStop, OffsetDateTime dateTime, String transportMode) {
+        this.fromStop = fromStop;
+        this.toStop = toStop;
+        this.viaStop = viaStop;
+        this.dateTime = dateTime;
+        this.transportMode = transportMode;
+    }
+
+    public QueryObject(int fromStop, int toStop, int viaStop, ArrayList<String> transportModes) {
+        this.fromStop = fromStop;
+        this.toStop = toStop;
+        this.viaStop = viaStop;
+        this.transportModes = transportModes;
+    }
+
+    public QueryObject(int fromStop, int toStop, int viaStop, OffsetDateTime dateTime, ArrayList<String> transportModes) {
+        this.fromStop = fromStop;
+        this.toStop = toStop;
+        this.viaStop = viaStop;
+        this.dateTime = dateTime;
+        this.transportModes = transportModes;
+    }
+
+    public int getFromStop() {
+        return fromStop;
+    }
+
+    public int getToStop() {
+        return toStop;
+    }
+
+    public int getViaStop() {
+        return viaStop;
+    }
+
+    public OffsetDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getTransportMode() {
+        return transportMode;
+    }
+
+    public ArrayList<String> getTransportModes() {
+        return transportModes;
+    }
+}
