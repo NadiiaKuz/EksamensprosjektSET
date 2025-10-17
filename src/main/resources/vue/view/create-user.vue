b8b77bd5c808e2c4920eb54d2fc0820fcbd6b76b
katTheCatmom <katrine.hoel@gmail.com>
2025-10-17 16:33:37 +0200

    opprettet vue-filen og css-arket nødvendig

---
+++ src/main/resources/vue/view/create-user.vue
@@ -0,0 +1,11 @@
+<template id="create-user-template">
+
+</template>
+
+<link rel="stylesheet" href="/css/global-style.css">
+<link rel="stylesheet" href="/css/create-user.css">
+<script>
+  app.component("create-user", {
+    template: "#create-user-template"
+  });
+</script>
