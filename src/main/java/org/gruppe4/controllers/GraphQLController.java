116efcbead55276f4294313e601f5731031c00f9
aymanbb <aymancool30@gmail.com>
2025-10-28 09:14:40 +0100

    Controller + created test directory
    Nesten ferdig med getTransportRoutes metoden, blir en del if-tester for å kunne opprette Query objekter med forskjellige parametere (refactor kommer senere).
    
    Lagde også en test directory og flyttet kode fra GraphQLRequestApplication til test dir.

--- src/main/java/org/gruppe4/controllers/GraphQLController.java
+++ src/main/java/org/gruppe4/controllers/GraphQLController.java
@@ -5,7 +5,8 @@ import graphql.query.GraphQLQuery;
 import graphql.query.QueryObject;
 import io.javalin.http.Context;
 
-import java.time.OffsetDateTime;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class GraphQLController {
@@ -16,15 +17,50 @@ public class GraphQLController {
     }
 
     public void getTransportRoutes(Context context) {
-        int fromStopId = Integer.parseInt(context.formParam("fromStopId"));
-        int toStopId = Integer.parseInt(context.formParam("toStopId"));
-        int viaStopId = Integer.parseInt(context.formParam("viaStopId"));
+        String fromStopId = context.formParam("fromStopId");
+        String toStopId = context.formParam("toStopId");
 
-        // dateTime kan deles opp i dato og klokkeslett, deretter parses til OffsetDateTime
-        OffsetDateTime dateTime;
+        String viaStopId = context.formParam("viaStopId");
 
-        // bruker skal kunne velge 1 eller flere transportmidler, finn en passende context metode
-        String transportMode;
+        String dateString = context.formParam("date");
+        String timeString = context.formParam("time");
+
+        ArrayList<String> transportTypes = new ArrayList<>();
+        transportTypes.add(context.formParam("transportTypes"));
+
+        if (fromStopId.isEmpty() || toStopId.isEmpty()) {
+            context.result("Please enter a valid stop ID");
+        } else {
+            int fromStopIdInt = Integer.parseInt(fromStopId);
+            int toStopIdInt = Integer.parseInt(toStopId);
+            int viaStopIdInt = Integer.parseInt(viaStopId);
+
+            LocalDate date;
+            LocalTime time;
+
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+
+            if (!dateString.isEmpty() || !timeString.isEmpty()) {
+                if (dateString.isEmpty()) {
+                    date = LocalDate.now();
+                } else {
+                    date = LocalDate.parse(dateString, timeFormatter);
+                }
+
+                if (timeString.isEmpty()) {
+                    time = LocalTime.now();
+                } else {
+                    time = LocalTime.parse(timeString, dateFormatter);
+                }
+
+                LocalDateTime dateTime = LocalDateTime.of(date, time);
+                OffsetDateTime offsetDateTime = OffsetDateTime.of(dateTime, ZoneOffset.ofHours(1));
+
+            }
+
+
+        }
     }
 
     public void checkForDelayedTransports(Context context) {
