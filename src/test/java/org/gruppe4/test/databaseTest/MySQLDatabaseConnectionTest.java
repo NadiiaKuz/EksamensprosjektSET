ea37d15f24014714a62d99593955aff2d08f8da4
BBrasa <brageb@hiof.no>
2025-10-30 16:37:13 +0100

    Har laget et par integrasjonstester for database med bruk av testcontainere gjennom Docker Desktop og lagt til nødvendige dependencies i pom.xml

--- src/test/java/org/gruppe4/test/databaseTest/MySQLDatabaseConnectionTest.java
+++ src/test/java/org/gruppe4/test/databaseTest/MySQLDatabaseConnectionTest.java
@@ -8,18 +8,22 @@ import java.sql.Connection;
 
 public class MySQLDatabaseConnectionTest extends TestDatabase {
 
+    //Lager container til bruk i Docker Desktop
     private static final MySQLContainer<?> mysql =
             new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
                     .withDatabaseName("testdb")
                     .withUsername("testuser")
                     .withPassword("testpassword");
 
+    //Lager et objekt av MySQLDatabaseConnection klassen
     private MySQLDatabaseConnection dbConnection;
 
+    //Lager override av startDB() metoden, slik at den kan brukes med testcontaineren
     @Override
     public Connection startDB() throws Exception {
         mysql.start();
 
+        //Starter docker containeren, dette krever at man har Docker Desktop installert
         dbConnection = new MySQLDatabaseConnection(
                 mysql.getJdbcUrl(),
                 mysql.getUsername(),
@@ -31,7 +35,7 @@ public class MySQLDatabaseConnectionTest extends TestDatabase {
         createDummyData();
         return connection;
     }
-
+    //Lukker testdatabasen
     @Override
     public void stopDB() throws Exception {
         dbConnection.stopDB();
