ee1821ff1d932832076987b802ed484696da9192
katTheCatmom <katrine.hoel@gmail.com>
2025-11-07 11:35:37 +0100

    Update create-user.vue
    Inputvalidering for fornavn, etternavn og mail

--- src/main/resources/vue/view/create-user.vue
+++ src/main/resources/vue/view/create-user.vue
@@ -1,18 +1,19 @@
 <template id="create-user-template">
   <form action="" method="post">
+
     <h1>Opprett ny bruker</h1>
+
     <label for="firstName" >Fornavn*:
       <input type="text" id="firstName" placeholder="Kari*" maxlength="45" pattern="[A-Za-zæøåÆØÅ]{2,}"required>
     </label>
+
     <label for="lastName">Etternavn:
       <input type="text" id="lastName" placeholder="Nordmann" maxlength="45" pattern="[A-Za-zæøåÆØÅ]{2,}">
     </label>
 
-
     <label for="mail">E-postadresse*:
       <input type="email" id="mail" placeholder="Kari.Nordmann@hiof.no" pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" required>
     </label>
-
 
     <label for="userType" id="type">Velg reisende:
       <select name="reisende" id="userType" required>
@@ -27,8 +28,7 @@
     <label for="password">Passord
       <input type="password" id="password" required>
     </label>
-
-
+    
     <label for="vilkaar" id="terms">
       <input type="checkbox" id="vilkaar" required>
       Ja, jeg godtar vilkårene.
